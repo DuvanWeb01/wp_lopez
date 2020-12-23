a0a7a32700812ac61aea826e4bebf2a929f9a788
Duvan-rd <desarrolloweb@univercity.com.co>
2020-12-23 09:12:45 -0500

    Primer commit

---
+++ master-template-woo-child/assets/js/custom-child.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function($){
+    $('.lp-carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+    $('.lp-carousel-proyects').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        autoplay: true,
+        dots: true,
+    });
+
+    $('.lp-tipos__car').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+      autoplay: true,
+      dots: false,
+    });
+
+    $('#lp-galeria a').append('<i class="far fa-eye"></i>');
+    $('.add_to_wishlist').html('<i class="far fa-heart"></i>');
+    
+});
---
+++ master-template-woo-child/template-parts/footer/bottom-footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying bottom footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Master_Template
+ */
+
+global $geniorama
+?>
+
+<div class="bottom-footer p-2 pt-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 text-center">
+				<p>2020 © López publicidad</p>
+			</div>
+			<div class="col-12 col-md-6 text-center">
+				<p>Desarrollado por <a href="#">Balboa</a></p>
+			</div>
+		</div>
+	</div>
+</div>
