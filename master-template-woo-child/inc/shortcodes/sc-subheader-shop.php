a61d8ebbd5c9aa378572bf45a7fae77d3cf86ce2
Angel Burgos Desktop <angelpublicista.1@gmail.com>
2020-12-26 11:27:56 -0500

    Ajuste input subheader

--- master-template-woo-child/inc/shortcodes/sc-subheader-shop.php
+++ master-template-woo-child/inc/shortcodes/sc-subheader-shop.php
@@ -7,7 +7,7 @@ if(!function_exists('lp_subheader_shop_func')){
         <div class="lp-subheader-shop text-center py-5" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-subheader-shop.jpg');">
             <div class="container">
                 <h1 class="header-title">TIENDA VIRTUAL <hr class="title-divider"></h1>
-                <form action="" class="lp-search-form" role="search" method="get" action="<?php echo home_url('/'); ?>">>
+                <form action="" class="lp-search-form" role="search" method="get" action="<?php echo home_url('/'); ?>">
                     <input type="search" class="form-control" placeholder="¿Qué estás buscando?" value="<?php echo get_search_query(); ?>" name="s">
                     <input type="hidden" name="post_type" value="product">
                     <button type="submit"><i class="fas fa-search"></i></button>
